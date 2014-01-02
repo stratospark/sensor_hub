127f6d28c6df0b12a5839e581d763e69f0ab655f
Patrick Rodriguez <patr84@gmail.com>
2014-01-01 21:23:56 -0800

    Made settings test reload settings module for full test coverage

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,13 +1,15 @@
-from mock import patch
 import settings
+from mock import patch
 
 
 class TestSettings(object):
 
     def test_pi_environment(self):
         with patch.dict('settings.os.environ', {'USER': 'pi'}):
+            reload(settings)
             settings.API_BASE_URL.find('localhost') == -1
 
     def test_local_environment(self):
         with patch.dict('settings.os.environ', {'USER': 'notpi'}):
+            reload(settings)
             settings.API_BASE_URL.find('localhost') >= 0
